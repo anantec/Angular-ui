2596cc0075e3d275f63175cff6bb5e546ed96fb3
Anant <asingh1037125@gmail.com>
2024-07-06 15:24:17 +0530

    some new pages

--- src/app/Components/Pages/payment/payment.component.ts
+++ src/app/Components/Pages/payment/payment.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Step3Component } from '../../../assets/progres-bar/step-3/step-3.component';
 import { BoatComponent } from '../../../assets/boat/boat.component';
+import { SubMainComponent } from './sub-main/sub-main.component';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [Step3Component, BoatComponent ],
+  imports: [Step3Component, BoatComponent, SubMainComponent],
   templateUrl: './payment.component.html',
   styles: ''
 })
